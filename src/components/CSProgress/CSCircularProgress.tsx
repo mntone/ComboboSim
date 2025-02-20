98601de8666c13f9720af41989a19a80374c5dfa
mntone <901816+mntone@users.noreply.github.com>
2025-02-21 01:35:04 +0900

    fix: improve accessibility of CSCircularProgress

--- src/components/CSProgress/CSCircularProgress.tsx
+++ src/components/CSProgress/CSCircularProgress.tsx
@@ -1,9 +1,18 @@
+import { msg } from '@lingui/core/macro'
+import { useLingui } from '@lingui/react/macro'
+
 import * as styles from './styles.css'
 import type { CSCircularProgressProps } from './types'
 
 function CSCircularProgress({ size }: CSCircularProgressProps) {
+	const { t } = useLingui()
 	return (
-		<svg className={styles.circular[size ?? 'md']} viewBox='-8 -8 16 16'>
+		<svg
+			aria-label={t(msg`Loading...`)}
+			className={styles.circular[size ?? 'md']}
+			role='progressbar'
+			viewBox='-8 -8 16 16'
+		>
 			<circle r={6} />
 		</svg>
 	)
